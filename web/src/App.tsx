9ae8e43914da0fb5d888b24279532e9f9681431e
leondavidtb <leondavidtb@gmail.com>
2023-01-18 11:13:56 -0300

    feat: web - configuring tailwind - refactoring App - creating Header and Summary components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
+import { Header } from "./components/Header";
+import { Summary } from "./components/Summary";
 import "./styles/global.css";
-import { Habit } from "./components/Habit";
 
-function App() {
-  return <Habit completed={3} />;
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <Summary />
+      </div>
+    </div>
+  );
 }
-
-export default App;

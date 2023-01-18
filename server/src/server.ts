9bbb91c23d39e619816204b76c76e28fba77a4e7
leondavidtb <leondavidtb@gmail.com>
2023-01-18 08:21:31 -0300

    feat: backend - include HabitWeekDays, Day and DayHabit tables and their relationship - creating seed.ts - generating ERD diagram - creating post habits and get days routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,12 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
-app.register(cors);
 
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-});
+app.register(cors);
+app.register(appRoutes);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server is running...");

4106bcf0c2b116b2d4f52c92c0603ea152632256
leondavidtb <leondavidtb@gmail.com>
2023-01-20 07:52:30 -0300

    feat: mobile - fetching summary data from api - creating new habit and save to databse

--- web/src/components/Summary.tsx
+++ web/src/components/Summary.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axtios";
 import { generateYearDates } from "../utils/generate-year-dates";
 import { HabitDay } from "./HabitDay";
 
@@ -7,7 +10,22 @@ const summaryDates = generateYearDates();
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[];
+
 export function Summary() {
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    api.get("/summary").then((response) => {
+      setSummary(response.data);
+    });
+  }, []);
+
   return (
     <div className="w-full flex ">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -24,11 +42,16 @@ export function Summary() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(date).isSame(day.date, "day");
+          });
+
           return (
             <HabitDay
               key={date.toString()}
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           );
         })}

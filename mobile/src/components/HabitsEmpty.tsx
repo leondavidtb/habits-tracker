1badb5a67be53c2c430700bc0c1444d45384c80d
leondavidtb <leondavidtb@gmail.com>
2023-01-22 09:11:44 -0300

    refactor: mobile - fixed issue of not listing activities correctly on current day - refactor imports

--- mobile/src/components/HabitsEmpty.tsx
+++ mobile/src/components/HabitsEmpty.tsx
@@ -1,18 +1,18 @@
-import { useNavigation } from "@react-navigation/native";
 import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export function HabitsEmpty() {
-  const { navigate } = useNavigation()
+  const { navigate } = useNavigation();
 
   return (
     <Text className="text-zinc-400 text-base">
-      Você ainda não está monitorando nenhum hábito {' '}
+      Você ainda não está monitorando nenhum hábito{" "}
       <Text
         className="text-violet-400 text-base underline active:text-violet-500"
-        onPress={() => navigate('new')}
+        onPress={() => navigate("new")}
       >
         comece cadastrando um.
       </Text>
     </Text>
-  )
+  );
 }
--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { useRoute } from "@react-navigation/native";
 import { Alert, ScrollView, Text, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
 import dayjs from "dayjs";
 import clsx from "clsx";
 
@@ -8,7 +8,7 @@ import { api } from "../lib/axios";
 import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 import { BackButton } from "../components/BackButton";
-import { ProgressBar } from "../components/Progress.Bar";
+import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
 import { Loading } from "../components/Loading";
 import { HabitsEmpty } from "../components/HabitsEmpty";
@@ -28,30 +28,39 @@ interface DayInfoProps {
 export function Habit() {
   const [loading, setLoading] = useState(true);
   const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null);
-  const [completedHabits, setCompletedHabits] = useState<string[]>([])
+  const [completedHabits, setCompletedHabits] = useState<string[]>([]);
 
-  const route = useRoute()
+  const route = useRoute();
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
-  const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
-  const dayOfWeek = parsedDate.format('dddd');
-  const dayAndMonth = parsedDate.format('DD/MM');
+  const dateParams = dayjs(date).format("YYYY-MM-DDT03:00:00.000z");
+  const isDateInPast = parsedDate.endOf("day").isBefore(new Date());
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayAndMonth = parsedDate.format("DD/MM");
 
-  const habitsProgress = dayInfo?.possibleHabits?.length ? generateProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length) : 0
+  const habitsProgress = dayInfo?.possibleHabits?.length
+    ? generateProgressPercentage(
+        dayInfo.possibleHabits.length,
+        completedHabits.length
+      )
+    : 0;
 
   async function fetchHabits() {
     try {
-      setLoading(true)
+      setLoading(true);
 
-      const response = await api.get('/day', { params: { date } });
+      const response = await api.get("/day", { params: { date: dateParams } });
       setDayInfo(response.data);
-      setCompletedHabits(response.data.completedHabits ?? [])
+      setCompletedHabits(response.data.completedHabits ?? []);
     } catch (error) {
-      console.log(error)
-      Alert.alert('Ops', 'Não foi possível carregar as informações dos hábitos.')
+      console.log(error);
+      Alert.alert(
+        "Ops",
+        "Não foi possível carregar as informações dos hábitos."
+      );
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -60,24 +69,24 @@ export function Habit() {
       await api.patch(`/habits/${habitId}/toggle`);
 
       if (completedHabits?.includes(habitId)) {
-        setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId));
+        setCompletedHabits((prevState) =>
+          prevState.filter((habit) => habit !== habitId)
+        );
       } else {
-        setCompletedHabits(prevState => [...prevState, habitId]);
+        setCompletedHabits((prevState) => [...prevState, habitId]);
       }
     } catch (error) {
-      console.log(error)
-      Alert.alert('Ops', 'Não foi possível atualizar o status do hábito.')
+      console.log(error);
+      Alert.alert("Ops", "Não foi possível atualizar o status do hábito.");
     }
   }
 
   useEffect(() => {
-    fetchHabits()
-  }, [])
+    fetchHabits();
+  }, []);
 
   if (loading) {
-    return (
-      <Loading />
-    )
+    return <Loading />;
   }
 
   return (
@@ -98,13 +107,14 @@ export function Habit() {
 
         <ProgressBar progress={habitsProgress} />
 
-        <View className={clsx("mt-6", {
-            ['opacity-50']: isDateInPast
-          })}>
-          {
-            dayInfo?.possibleHabits ?
-            dayInfo.possibleHabits?.map(habit => (
-              <Checkbox 
+        <View
+          className={clsx("mt-6", {
+            ["opacity-50"]: isDateInPast,
+          })}
+        >
+          {dayInfo?.possibleHabits ? (
+            dayInfo.possibleHabits?.map((habit) => (
+              <Checkbox
                 key={habit.id}
                 title={habit.title}
                 checked={completedHabits?.includes(habit.id)}
@@ -112,19 +122,17 @@ export function Habit() {
                 disabled={isDateInPast}
               />
             ))
-            : 
+          ) : (
             <HabitsEmpty />
-          }
+          )}
         </View>
 
-        {
-          isDateInPast && (
-            <Text className="text-white mt-10 text-center">
-              Você não pode editar hábitos de uma data passada.
-            </Text>
-          )
-        }
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada.
+          </Text>
+        )}
       </ScrollView>
     </View>
-  )
+  );
 }
